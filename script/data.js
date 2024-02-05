1bd1c5ce2b315e628ca636fa2e8d4639b2b6e46b
Abd Al Rhman Alkhpe <128310146+AbdALrhmanxxx@users.noreply.github.com>
2024-02-05 19:36:00 +0300

    Shoes-Project

---
+++ script/data.js
@@ -0,0 +1,52 @@
+export const products = [
+  {
+    name: "retro-shoes",
+    priceCents: 10000,
+    image: "shoes",
+  },
+  {
+    name: "retro-shoes",
+    priceCents: 10000,
+    image: "shoes1",
+  },
+  ,
+  {
+    name: "retro-shoes2",
+    priceCents: 10000,
+    image: "shoes2",
+  },
+  ,
+  {
+    name: "retro-shoes3",
+    priceCents: 10000,
+    image: "shoes3",
+  },
+  ,
+  {
+    name: "retro-shoes4",
+    priceCents: 10000,
+    image: "shoes4",
+  },
+  ,
+  {
+    name: "retro-shoes5",
+    priceCents: 10000,
+    image: "shoes5",
+  },
+  {
+    name: "retro-shoes6",
+    priceCents: 10000,
+    image: "shoes6",
+  },
+  {
+    name: "retro-shoes6",
+    priceCents: 10000,
+    image: "shoes7",
+  },
+  ,
+  {
+    name: "retro-shoes6",
+    priceCents: 10000,
+    image: "shoes8",
+  },
+];
